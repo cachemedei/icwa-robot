273448a2effa0a707f2c5f39556067ad9f507a10
cachemedei <cachemedei@gmail.com>
2025-03-01 10:46:13 +0800

    added move

--- toy_robot.py
+++ toy_robot.py
@@ -14,8 +14,35 @@ class Robot():
         self.posY = y
         self.face = facing
 
+    # Move Sam 1 space
+    def move(self):
+        # facing north => increase Y
+        if self.face == "North":
+            self.posY += 1
+            if self.posY > 4:
+                self.posY = 4
+
+        # facing south => decrease Y
+        if self.face == "South":
+            self.posY -= 1
+            if self.posY < 0:
+                self.posY = 0
+
+        # facing east => increase X
+        if self.face == "East":
+            self.posX += 1
+            if self.posX > 4:
+                self.posX = 4
+
+        # facing west => decrease X
+        if self.face == "West":
+            self.posX -= 1
+            if self.posX < 0:
+                self.posX = 0
 
 sam = Robot()
 sam.report()
 sam.place(2, 4, "East")
+sam.report()
+sam.move()
 sam.report()
